2aa752730b42f81502c6f1bde606784d5813598e
pedropiresic <pedro.pires@industrycare.com.br>
2021-02-23 17:23:58 -0300

    Atualizando repositório

---
+++ TypeScript/01-alurabank/alurabank/app/js/models/Negociacao.js
@@ -0,0 +1,30 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+    }
+
+    get data() {
+
+        return this._data;
+    }
+
+    get quantidade() {
+
+        return this._quantidade;
+    }
+
+    get value() {
+
+        return this._valor;
+    }
+
+    get volume() {
+
+        return this._quantidade * this._valor;
+    }
+}
